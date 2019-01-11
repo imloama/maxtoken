7f7d9698c8ecfde2d2493c404fc49a57e9aac40b
马兆永 <mazhaoyong@gmail.com>
2019-01-11 22:16:56 +0800

    bug: re generate files

--- android/app/src/main/java/com/github/imloama/maxtoken/MainActivity.java
+++ android/app/src/main/java/com/github/imloama/maxtoken/MainActivity.java
@@ -4,17 +4,10 @@ import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
-public class MainActivity extends FlutterActivity implements KeystoreStorage{
+public class MainActivity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     GeneratedPluginRegistrant.registerWith(this);
-    WalletManager.storage = this;
-    WalletManager.scanWallets();
   }
-
-   public File getKeystoreDir() {
-        return this.getFilesDir();
-    }
-
 }
