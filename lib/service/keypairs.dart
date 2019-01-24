87bd4b7586555eff59a64dfcf16e4cda3daa2452
imloama <imloama@gmail.com>
2019-01-24 17:09:46 +0800

    feat: keypair

--- lib/service/keypairs.dart
+++ lib/service/keypairs.dart
@@ -1,6 +1,26 @@
+import 'package:maxtoken/service/blockchain.dart';
+import 'package:stellar/stellar.dart';
 
 class KeyPairs{
 
-  
+  static KeyPairData random(BlockChainType type){
+    if(type == BlockChainType.Stellar){
+      KeyPair kp = KeyPair.random();
+      return KeyPairData(
+        publicKey: kp.accountId,
+        secret: kp.secretSeed
+      )
+    }
+  }
+
+}
+
+class KeyPairData{
+
+  final String publicKey;
+  final String secret;
+
+  KeyPairData({this.publicKey, this.secret});
+
 
 }
