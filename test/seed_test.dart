4cb95c1a10dd245193b9a5e840c29b41faa02f3d
imloama <imloama@gmail.com>
2019-01-30 16:27:08 +0800

    test: stellar hd wallet ok

--- test/seed_test.dart
+++ test/seed_test.dart
@@ -1,5 +1,6 @@
 import 'package:maxtoken/service/blockchain.dart';
 import 'package:pointycastle/digests/sha512.dart';
+import 'package:pointycastle/export.dart';
 import 'package:stellar/stellar.dart';
 import 'package:bip39/bip39.dart' as bip39;
 import 'package:bip32/bip32.dart' as bip32;
@@ -78,6 +79,26 @@ Uint8List createUint8ListFromString(String s) {
   return ret;
 }
 
+const HIGHEST_BIT = 0x80000000;
+
+Uint8List drive(Uint8List seed, Uint8List chainCode, int index){
+  var y = 2147483648 + index;
+  Uint8List data = new Uint8List(37);
+  data[0] = 0x00;
+  data.setRange(1, 33, seed);
+  data.buffer.asByteData().setUint32(33, y);
+
+  SHA512Digest digest = SHA512Digest();
+  KeyParameter parameter = new KeyParameter(chainCode);
+  HMac hmac = HMac(digest,128);
+  hmac.init(parameter);
+  hmac.reset();
+  hmac.update(data, 0, data.length);
+  var output = Uint8List(64);
+  hmac.doFinal(output, 0);
+  return output;
+}
+
 void testStellar3(){
   //数据来自https://github.com/stellar/stellar-protocol/blob/master/ecosystem/sep-0005.md
   final mnemonic = "illness spike retreat truth genius clock brain pass fit cave bargain toe";
@@ -86,18 +107,34 @@ void testStellar3(){
   // var key = utf8.encode("ed25519 seed");
   final key = createUint8ListFromString("ed25519 seed");
   KeyDerivator derivator = new KeyDerivator("SHA-512/HMAC/PBKDF2");
-  CipherParameters parameters =new Pbkdf2Parameters(key, 100, 64);
-  derivator.init(parameters);
-  final output = derivator.process(seed);
-  print(HEX.encode(output));
+  // CipherParameters parameters =new Pbkdf2Parameters(key, 100, 64);
+  KeyParameter parameter = new KeyParameter(key);
+  // derivator.init(parameter);
+  SHA512Digest digest = SHA512Digest();
+  // final output = derivator.process(seed);
+  // derivator.process(seed);
+  HMac hmac = HMac(digest,128);
+  hmac.init(parameter);
+  hmac.reset();
+  hmac.update(seed, 0, seed.length);
+  var output = Uint8List(64);
+  hmac.doFinal(output, 0);
+  print(HEX.encode(output));//correct
   // //恒星 m/44'/148'/0' key:
   // byte[] i44 = derive(extendedKey.getMaster().getRawPrivateKey(), extendedKey.getChainCode(), 44);
   // byte[] i148 = derive(Arrays.copyOfRange(i44, 0, 32), Arrays.copyOfRange(i44, 32, 64), 148);
   // byte[] i0 = derive(Arrays.copyOfRange(i148, 0, 32), Arrays.copyOfRange(i148, 32, 64), 0);
-  final root = bip32.BIP32.fromSeed(output);
-  var node = root.derivePath("m/44'/148'/0'");
-  KeyPair kp = KeyPair.fromSecretSeedList(node.privateKey);
+  var left = output.sublist(0,32);
+  var right = output.sublist(32,64);
+  print("left:"+left.length.toString()+","+HEX.encode(left));
+  print("right:"+right.length.toString()+","+HEX.encode(right));
+
+  Uint8List i44 = drive(left, right, 44);
+  var i148 = drive(i44.sublist(0,32), i44.sublist(32), 148);
+  var i0 = drive(i148.sublist(0,32), i148.sublist(32), 0);
+  KeyPair kp = KeyPair.fromSecretSeedList(i0);
   print(kp.accountId);
+
 }
 
 Future testBtc() async{
