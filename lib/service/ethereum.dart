efacfc67ee6d67bc07f4b0427d8c340ae9196467
max <imloama@gmail.com>
2019-02-15 21:49:01 +0800

    feat: service

---
+++ lib/service/ethereum.dart
@@ -0,0 +1,21 @@
+import 'package:web3dart/web3dart.dart';
+import 'package:maxtoken/service/service.dart';
+
+class EthereumService {
+
+
+  
+
+}
+
+class EthereumAddress extends Address{
+
+
+  @override
+  String getPublicAddress() {
+    
+    return null;
+  }
+
+
+}
---
+++ lib/service/service.dart
@@ -0,0 +1,14 @@
+
+
+
+abstract class Address{
+   //助记词
+  String mnemonic = null;
+  //公钥
+  String address = null;
+  //私钥
+  String secret = null;
+
+  String getPublicAddress();
+
+}
