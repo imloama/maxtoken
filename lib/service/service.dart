b09e1871609368e65a323d2bb2cdcc818437202f
max <imloama@gmail.com>
2019-02-16 11:49:29 +0800

    feat: ethereum address

--- lib/service/service.dart
+++ lib/service/service.dart
@@ -4,11 +4,24 @@
 abstract class Address{
    //助记词
   String mnemonic = null;
+  int index = 0;
   //公钥
   String address = null;
   //私钥
   String secret = null;
 
-  String getPublicAddress();
+  Address.fromPublicAddress(String address){
+    this.address = address;
+  }
+
+  Address.fromSecret(String secret){
+    this.secret = secret;
+  }
+
+  Address.fromMnemonic(String mnemonic,[int index = 0]){
+    this.mnemonic = mnemonic;
+    this.index = index;
+  }
+
 
 }
